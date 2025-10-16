dd3037a4e22d8ffed9b6dce723d135b1505e4384
inesuribeb <inesuribeb@gmail.com>
2025-10-16 18:47:06 +0200

    componente worker

---
+++ mi-aplicacion/src/components/Worker/Worker.jsx
@@ -0,0 +1,11 @@
+import './Worker.css';
+
+function Worker({ t , workerName, workerRole }) {
+    return (
+        <section className='worker-section'>
+
+        </section>
+    );
+}
+
+export default Worker;
