85bc5bea5368bf35da576db1e6d73f3f02dcf3bd
inesuribeb <inesuribeb@gmail.com>
2025-10-21 22:38:52 +0200

    new footer

--- mi-aplicacion/src/components/Footer/Footer.jsx
+++ mi-aplicacion/src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import { NavLink, useLocation } from 'react-router-dom'; // Cambia Link por NavLink
 import { useLanguage } from '../../contexts/LanguageContext';
+import { Instagram, Linkedin } from 'lucide-react'; // Importa los iconos
+import { FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { RiInstagramLine, RiLinkedinLine } from 'react-icons/ri';
+import { AiOutlineInstagram, AiOutlineLinkedin } from 'react-icons/ai';
+
+
 import './Footer.css';
 
 function Footer() {
@@ -9,9 +15,15 @@ function Footer() {
     const currentPath = location.pathname;
     const homeRoute = getRoute('home');
     const managementRoute = getRoute('management');
+    const arhitectureRoute = getRoute('architecture');
+    const aboutUsRoute = getRoute('aboutUs');
+    const contactRoute = getRoute('contact');
 
     const isDarkFooter = currentPath === homeRoute ||
         currentPath === managementRoute ||
+        currentPath === arhitectureRoute ||
+        currentPath === aboutUsRoute ||
+        currentPath === contactRoute ||
         currentPath === '/';
 
     const navigationLinks = [
@@ -32,27 +44,45 @@ function Footer() {
     return (
         <section className={`footer-container ${isDarkFooter ? 'footer-dark' : 'footer-light'}`}>
 
+
+            <div className='footer-f2'>
+                <img src="/Logos/jotaBlanco.png" alt="" />
+            </div>
+
+            {/* <div className='footer-logos'>
+                <a href="https://instagram.com/jota.arquitectura_" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                    <FaInstagram size={24} />
+                </a>
+                <a href="https://linkedin.com/company/jota-arquitectura" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                    <FaLinkedin size={24} />
+                </a>
+            </div> */}
+
             <div className='footer-f1'>
-                <div className='col'>
+                {/* <div className='col'>
                     <h1>{t('oficina')}</h1>
                     <p>{t('address1')}</p>
                     <p>{t('address2')} •</p>
                     <p>{t('address3')}</p>
-                </div>
-                <div className='col'>
+                    <p></p>
+                    <p>jota@arquitectura.es</p>
+                    <p>+34 666 66 66 66</p>
+                    
+                </div> */}
+                {/* <div className='col'>
                     <h1>{t('contacto')}</h1>
                     <p>+34 666 66 66 66</p>
                     <p>jota@arquitectura.es</p>
-                </div>
-                <div className='col'>
+                </div> */}
+                {/* <div className='col'>
                     <h1>{t('rrss')}</h1>
                     <p>Instagram</p>
                     <p>Linkedin</p>
-                </div>
+                </div> */}
+                {/* <div className='col'>
+                </div> */}
                 <div className='col'>
-                </div>
-                <div className='col'>
-                    <h1>{t('menu')}</h1>
+                    {/* <h1>{t('menu')}</h1> */}
                     <nav className='footer-nav'>
                         {navigationLinks.map(link => (
                             <NavLink
@@ -68,26 +98,22 @@ function Footer() {
                     </nav>
                 </div>
 
+                
+
             </div>
 
-            <div className='footer-f2'>
-                <div className='col'>
-                    <img src="/Images/logo.png" alt="" />
-                </div>
-
-                <div className='col'>
-                    <h2 className='jota-name'>Jota</h2>
-                </div>
-
-                <div className='col'>
-                </div>
-
-                <div className='col'>
-                    <h2 className='arqAndgest'>arquitectura y gestión</h2>
-                </div>
-                <div className='col'>
-                </div>
+            <div className='footer-logos'>
+                <a href="https://www.instagram.com/jota.arquitectura_/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                    <FaInstagram size={24} />
+                </a>
+                <a href="https://www.linkedin.com/company/jotaarq/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                    <FaLinkedin size={24} />
+                </a>
             </div>
+
+            {/* <div className='footer-f2'>
+                <img src="/Logos/jotaBlanco.png" alt="" />
+            </div> */}
 
             <div className='footer-f3'>
                 <div className='col3'>
